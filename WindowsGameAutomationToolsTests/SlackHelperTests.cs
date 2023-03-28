99995a622ed266d0e0152a87dd01e365470aef71
DESKTOP-0209OAO\peter <peter.beckfield@gmail.com>
2023-03-27 20:53:01 -0500

    More SlackHelper unit tests

--- WindowsGameAutomationToolsTests/SlackHelperTests.cs
+++ WindowsGameAutomationToolsTests/SlackHelperTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
 using WindowsGameAutomationTools.Files;
 using WindowsGameAutomationTools.Slack;
@@ -8,15 +9,51 @@ namespace WindowsGameAutomationToolsTests
     [TestClass]
     public class SlackHelperTests
     {
-        [TestMethod]
-        [ExpectedException(typeof(FileNotFoundException))]
-        public void MissingSlackProfileTest()
+        [TestInitialize()]
+        public void Startup()
         {
             string profileFilename = $"{SlackProfile.DEFAULT_FILENAME}{SerializationHelper.JSON_SUFFIX}";
             if (File.Exists(profileFilename))
             {
                 File.Delete(profileFilename);
             }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void MissingSlackProfileTest()
+        {
+            SlackHelper.SendMessageToChannel("Message");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void MissingChannelIdTest()
+        {
+            SlackProfile profile = new SlackProfile();
+            SerializationHelper.SerializeToJSON(SlackProfile.DEFAULT_FILENAME, profile);
+
+            SlackHelper.SendMessageToChannel("Message");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void MissingOauthTokenTest()
+        {
+            SlackProfile profile = new SlackProfile();
+            profile.ChannelId = "ChannelId";
+            SerializationHelper.SerializeToJSON(SlackProfile.DEFAULT_FILENAME, profile);
+
+            SlackHelper.SendMessageToChannel("Message");
+        }
+
+        [TestMethod]
+        public void SuccessTest()
+        {
+            SlackProfile profile = new SlackProfile();
+            profile.ChannelId = "ChannelId";
+            profile.OauthToken = "OauthToken";
+            SerializationHelper.SerializeToJSON(SlackProfile.DEFAULT_FILENAME, profile);
 
             SlackHelper.SendMessageToChannel("Message");
         }
